045e1ae6a08edc1386f1625d510bab35ea58dfd0
DeTrainNerd <dwspelde3@gmail.com>
2026-09-13 17:41:40 -0500

    allow easier extension of this class

--- src/main/java/org/patryk3211/powergrid/kinetics/motor/ElectricMotorBlockEntity.java
+++ src/main/java/org/patryk3211/powergrid/kinetics/motor/ElectricMotorBlockEntity.java
@@ -48,12 +48,12 @@ public class ElectricMotorBlockEntity extends GeneratingKineticBlockEntity imple
     @Nullable
     protected ThermalBehaviour thermalBehaviour;
 
-    private ElectricWire coil;
+    protected ElectricWire coil;
 
     private float generatedSpeed = 0;
 
-    private float avgSpeed;
-    private float load;
+    protected float avgSpeed;
+    protected float load;
 
     public ElectricMotorBlockEntity(BlockEntityType<?> typeIn, BlockPos pos, BlockState state) {
         super(typeIn, pos, state);
